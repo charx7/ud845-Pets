244b71f7467098cc48ebaa51e00b19796b00df1f
Carlos Huerta <carlos.huertaso@udlap.mx>
2017-11-14 00:00:43 -0600

    Modficacion al drop-down menu de la actividad de modificacion para que no salga Delete de una actividad de insercion de mascota

--- app/src/main/java/com/example/android/pets/EditorActivity.java
+++ app/src/main/java/com/example/android/pets/EditorActivity.java
@@ -47,7 +47,7 @@ import com.example.android.pets.data.PetProvider;
  * Allows user to create a new pet or edit an existing one.
  */
 public class EditorActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor> {
-    
+
     // Metodo que implementa la creacion del dialogo de alerta de cambios sin guardar
     private void showUnsavedChangesDialog(
             DialogInterface.OnClickListener discardButtonClickListener) {
@@ -157,6 +157,13 @@ public class EditorActivity extends AppCompatActivity implements LoaderManager.L
 
         if (currentPetUri == null){
             setTitle("Añadir una mascota");
+            // Codigo que oculta la parte del menu que no queremos de DELETE
+            // This is a new pet, so change the app bar to say "Add a Pet"
+            setTitle(getString(R.string.editor_activity_title_new_pet));
+
+            // Invalidate the options menu, so the "Delete" menu option can be hidden.
+            // (It doesn't make sense to delete a pet that hasn't been created yet.)
+            invalidateOptionsMenu();
         } else {
             setTitle("Editar una mascota");
             // Inicializacion del background thread para el metodo query
@@ -310,6 +317,18 @@ public class EditorActivity extends AppCompatActivity implements LoaderManager.L
         // Inflate the menu options from the res/menu/menu_editor.xml file.
         // This adds menu items to the app bar.
         getMenuInflater().inflate(R.menu.menu_editor, menu);
+        return true;
+    }
+
+    // Override a la preparacion del menu para que esconda el delete de una actividad de insertar pet
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        super.onPrepareOptionsMenu(menu);
+        // If this is a new pet, hide the "Delete" menu item.
+        if (mCurrentPetUri == null) {
+            MenuItem menuItem = menu.findItem(R.id.action_delete);
+            menuItem.setVisible(false);
+        }
         return true;
     }
 
